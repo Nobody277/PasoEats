4311febd2ce72258c89c9ec6740e86a9b1fd1275
Connor HW <chealeywolf@gmail.com>
2025-11-08 12:50:00 -0800

    User changes

--- User.java
+++ User.java
@@ -1,24 +1,59 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 abstract class User {
-    private int id;
-    private ArrayList<String> orders = new ArrayList<>();
+    // variables
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private ArrayList<String> orders;
+   
+    // constructors
+    public User(){
+        orders = new ArrayList<>();
+    }
+    public User(UUID id, String name, String username, String email){
+        orders = new ArrayList<>();
 
-    public int getId() {
-        return id;
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
     }
 
+    // getters & setters
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     public ArrayList<String> getOrders() {
         return orders;
     }
-
     public void setOrders(ArrayList<String> orders) {
         this.orders = orders;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 }
 
 // User -> getRestraunts -> View Menus -> Select what they want -> Place Order -> Order Created -> Sent to Driver -> Order Status display [Accepted, in-progress, Picked Up, Delivered] -> User Notified -> Order History Updated -> User/driver can view past orders
