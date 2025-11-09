8bcdc0891eedb31bc23ab3a4797cfbf62c0601cd
Nobody277 <nobody.272.72@gmail.com>
2025-11-08 21:33:59 -0800

    Push to main

--- push.bat
+++ push.bat
@@ -1,23 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
 cls
-echo 1. Push now with "update"
+echo Available branches:
+echo.
+git branch
+echo.
+set /p branch=Enter branch name to push to (1 or 2): 
+echo.
+echo 1. Push with "update" message
 echo 2. Push with custom message
 echo.
-set/p choice=Enter your choice: 
+set /p choice=Enter your choice (1 or 2): 
+echo.
 if "%choice%"=="1" (
     git add .
     git commit -m "update"
-    git push origin develop
+    git push origin %branch%
 ) else if "%choice%"=="2" (
-    set/p msg=Enter your message:
+    set /p msg=Enter your commit message: 
     git add .
     git commit -m "!msg!"
-    git push origin develop
+    git push origin %branch%
 ) else (
     echo Invalid choice
-    pause
 )
-git commit -m "update"
-git push origin develop
 pause
